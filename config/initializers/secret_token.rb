2d132415c18ab70af54da99c0ea6feb941c6787f
Yasunori Endo <yasu@www1220ui.sakura.ne.jp>
2013-06-15 00:23:27 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fcount::Application.config.secret_token = 'd15f342b9528263d22675e5893ac3275cb15049736425a25a43e2112cf95cb90383e4323b173f758708e234b1c646a318fa676b7a8a5e53a4211cdc1794b96a3'
